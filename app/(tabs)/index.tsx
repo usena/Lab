1c7203196367d39006022b9f4e217b458e0de834
GooGaaGee <viriya.s@ligercambodia.org>
2026-04-24 09:29:01 +0700

    function

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,45 @@
-import { View, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Button, TextInput } from 'react-native';
 
 export default function HomeScreen() {
+  const [text, onChangeText] = React.useState('https://reactnative.dec/movies.json');
+
+  const [webData, setwebData] = useState(""); 
+  const request = new XMLHttpRequest();
+
+  function _handlePressButtonAsync() {
+    request.onreadystatechange = e => {
+      if (request.readyState !== 4) {
+        return;
+      }
+      if (request.status === 200) {
+        console.log('success', + request.responseText);
+        setwebData("Status: " + request.status + " " + request.statusText + " " + request.responseText + " " + request.response);
+      } else {
+          console.warn('error');
+          setwebData("Error: " + request.status + " " + request.statusText + " " + request.responseText);
+      }
+    };
+
+    request.open('GET', text);
+    request.send();
+  }
+
   return (
     <View style = {styles.containerColumn}>
       <View style = {styles.containerRow}>
+        <TextInput
+          style = {styles.input}
+          onChangeText={onChangeText}
+          value = {text}
+        />
+
         <Button
-          title = "Click me"
-          onPress={()=> alert("Stop pressing my button!")}
+          title = "Go Request"
+          onPress={()=> _handlePressButtonAsync()}
         />
       </View>
+      <text>{webData}</text>
     </View>
   );
 }
@@ -23,5 +54,9 @@ const styles = StyleSheet.create({
   containerRow: {
     flexDirection: 'row',
     justifyContent: 'space-between'
+  },
+
+  input: {
+    justifyContent: 'center',
   },
 });
